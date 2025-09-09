87b8d50be988555b690b50abb531f8203990ab68
hemantagrawal20 <hemantagrawal9084@gmail.com>
2025-09-09 16:52:12 +0530

    [Hemant ] add : added   BankAccount

---
+++ Operators/BankAccount.java
@@ -0,0 +1,44 @@
+package Operators;
+
+public class BankAccount {
+    private static String bankName = "State Bank of India";
+    private static int totalAccounts = 0;
+
+    private final int accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    public BankAccount(String accountHolderName, int accountNumber, double balance) {
+        this.accountHolderName = accountHolderName;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        totalAccounts++;
+    }
+
+    public static void getTotalAccounts() {
+        System.out.println("Total Accounts in " + bankName + ": " + totalAccounts);
+    }
+
+    public void displayDetails() {
+        if (this instanceof BankAccount) {
+            System.out.println("---- Account Details ----");
+            System.out.println("Bank Name: " + bankName);
+            System.out.println("Account Holder: " + accountHolderName);
+            System.out.println("Account Number: " + accountNumber);
+            System.out.println("Balance: " + balance);
+        } else {
+            System.out.println("Invalid Account Object!");
+        }
+    }
+
+    public static void main(String[] args) {
+        BankAccount acc1 = new BankAccount("Hemant", 101, 5000.0);
+        BankAccount acc2 = new BankAccount("Amit", 102, 10000.0);
+
+        acc1.displayDetails();
+        acc2.displayDetails();
+
+        BankAccount.getTotalAccounts();
+    }
+    
+}
