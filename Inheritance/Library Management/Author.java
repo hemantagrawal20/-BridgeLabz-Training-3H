c2249d04700261d9a4441235bf4dd66afdfdb159
hemantagrawal20 <hemantagrawal9084@gmail.com>
2025-09-16 14:21:14 +0530

    [Hemant]Added Inheritance

---
+++ Inheritance/Library Management/Author.java
@@ -0,0 +1,17 @@
+public class Author extends Book {
+    String name;
+    String bio;
+
+    public Author(String title, int publicationYear, String name, String bio) {
+        super(title, publicationYear);
+        this.name = name;
+        this.bio = bio;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Author Name: " + name);
+        System.out.println("Author Bio: " + bio);
+    }
+}
---
+++ Inheritance/Library Management/LibraryDemo.java
@@ -0,0 +1,6 @@
+public class LibraryDemo {
+    public static void main(String[] args) {
+        Author book1 = new Author("The Java Journey", 2023, "Hemant Sharma", "Passionate coder and lifelong learner.");
+        book1.displayInfo();
+    }
+}
